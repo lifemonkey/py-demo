756ed1b2e3110675f417b53ba9cfcabdf60f6525
Doan Quoc Hung <h.doan@Laudert.com>
2019-09-19 13:05:06 +0700

    Flask: refactor html, add email sender

--- hqd/flask/app.py
+++ hqd/flask/app.py
@@ -1,8 +1,24 @@
 from flask import *
+from flask_mail import *
+from random import *
+from email_validator import validate_email, EmailNotValidError
+
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'test secret key'
 
+# Flask-mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'hqd.auto.sender@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Learnbyheart8h'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+# Instantiate the Mail class
+mail = Mail(app)
+otp = randint(000000, 999999)
+
+# Users
 users = {
     'admin': 'admin',
     'hqd': 'hqd',
@@ -33,12 +49,33 @@ def login():
     return render_template('login.html', message=message)
 
 
-@app.route("/account")
+@app.route('/account')
 def account():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-
     return render_template('account.html')
+
+
+@app.route('/verify', methods=['POST'])
+def verify():
+    email = request.form.get('email', None)
+    try:
+        validate_email(email)
+        msg = Message('Activation code', sender='hqd.auto.sender@gmail.com', cc='h.doan@laudert.de', recipients=[email])
+        msg.body = 'Hi there, this is the mail sent by using the flask web application. Your activation code is ' + str(otp)
+        mail.send(msg)
+        return render_template('verify.html', email=email)
+    except EmailNotValidError:
+        flash("Email is not valid")
+        return redirect(url_for('account'))
+
+
+@app.route('/validate', methods=["POST"])
+def validate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp):
+        return "<h3>Email verified successfully</h3>"
+    return "<h3>failure</h3>"
 
 
 @app.route('/logout')
